4c3dcbb91ef2750e79a7507abe43d570a8115a17
kiransaiveerandi <kiransaiveerandi@gmail.com>
2025-09-19 16:44:34 +0530

    some changes

--- conversation.sh
+++ conversation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#conevrsation beetween two friends
+#chnage the name
+per1=raju
+per2=balu
 
-echo ramu:"hi ravi,how are you"
-echo ravi:"hi ramu,iam fine.how are you ?"
-echo ramu:"iam good"
-echo ravi:"what else"
+echo per1:"hi per2,how are you"
+echo per2:"hi per1,iam fine.how are you ?"
+echo per1:"iam good"
+echo per2:"what else"
